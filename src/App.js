fa8c2fc7d7ab449e9ba7b244f19e28c85fd212da
Dimitrios Tsikopoulos <dimitristsik@gmail.com>
2022-01-10 08:56:12 +0200

    components and display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
+import Figure from "./components/Figure";
+import Header from "./components/Header";
+import Word from "./components/Word";
+import WrongLetters from "./components/WrongLetters";
+
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
 
 function App() {
-  return <div className="app"></div>;
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+      </div>
+    </>
+  );
 }
 
 export default App;
